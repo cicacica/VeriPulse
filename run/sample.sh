b803625ea3883dc3067db33de66e87bf79a874fc
Cica Gustiani <cicagustiani@pm.me>
2026-04-09 15:11:36 +0200

    randomisation, data

--- run/sample.sh
+++ run/sample.sh
@@ -1,14 +1,9 @@
 # run in serial
  nohup bash -c '
- uv run python -u run_sampling_data.py -p 40 -m "GRAPE_AVG" -l 0.1 -i 3  &> p40l01-3.log &&
- uv run python -u run_sampling_data.py -p 40 -m "GRAPE_AVG" -l 0.1 -i 4  &> p40l01-4.log &&
- uv run python -u run_sampling_data.py -p 40 -m "GRAPE_AVG" -l 0.01 -i 2  &> p40l001-2.log &&
- uv run python -u run_sampling_data.py -p 40 -m "GRAPE_AVG" -l 0.01 -i 3  &> p40l001-3.log &&
- uv run python -u run_sampling_data.py -p 40 -m "GRAPE_AVG" -l 0.01 -i 4  &> p40l001-4.log &&
- uv run python -u run_sampling_data.py -p 40 -m "GRAPE_AVG" -l 0.01 -i 5  &> p40l001-5.log &&
- uv run python -u run_sampling_data.py -p 40 -m "GRAPE_AVG" -l 0.05 -i 2  &> p40l005-2.log &&
- uv run python -u run_sampling_data.py -p 40 -m "GRAPE_AVG" -l 0.05 -i 3  &> p40l005-3.log &&
- uv run python -u run_sampling_data.py -p 40 -m "GRAPE_AVG" -l 0.05 -i 4  &> p40l005-4.log &&
- uv run python -u run_sampling_data.py -p 40 -m "GRAPE_AVG" -l 0.05 -i 5  &> p40l005-5.log &&
- uv run python -u run_sampling_data.py -p 120 -m "GRAPE_AVG" -l 0.1 -n 5  &> p120l01.log 
+ uv run python -u run_sampling_data.py -p 60 -m "GRAPE_AVG" -l 0.05 -n 5  &> p60avg.log &&
+ uv run python -u run_sampling_data.py -p 80 -m "GRAPE" -n 5  &> p80grape.log &&
+ uv run python -u run_sampling_data.py -p 80 -m "CRAB" -n 5   &> p80crab.log && 
+ uv run python -u run_sampling_data.py -p 80 -m "GRAPE_AVG" -l 0.1 -n 5  &> p80avg.log &&
+ uv run python -u run_sampling_data.py -p 80 -m "GRAPE_AVG" -l 0.01 -n 5  &> p80avg.log &&
+ uv run python -u run_sampling_data.py -p 80 -m "GRAPE_AVG" -l 0.05 -n 5  &> p80avg.log 
  '  &> batch1.log &
