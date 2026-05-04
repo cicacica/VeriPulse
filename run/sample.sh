76feebd5b1c3fc66a38150b0245e50b1c3271ff2
Cica Gustiani <cicagustiani@pm.me>
2026-05-04 22:26:50 +0200

    40 almost complete

--- run/sample.sh
+++ run/sample.sh
@@ -1,4 +1,4 @@
 # run in serial
 nohup bash -c '
-uv run python -u run_sampling_data.py -p 40 -m "GRAPE_AVG" -dum -l 0.1 -n 5 &> logs/grape_avg.log 
+uv run python -u run_sampling_data.py -p 40 -m "GRAPE_AVG" -dum -l 0.026 -n 5 &> logs/grape_avg.log 
  '  &> logs/batch1.log &
