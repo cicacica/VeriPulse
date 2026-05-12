b02697c14e9fc2542e9963d842cadbd14ab5bd3f
Cica Gustiani <cicagustiani@pm.me>
2026-05-12 14:11:47 +0200

    sweep 70 dummyless

--- run/sample.sh
+++ run/sample.sh
@@ -1,4 +1,8 @@
 # run in serial
 nohup bash -c '
-uv run python -u run_sampling_data.py -p 40 -m "GRAPE_AVG" -dum -l 0.026 -n 5 &> logs/grape_avg.log 
+uv run python -u run_sampling_data.py -p 70 -m "GRAPE_AVG"  -l 0.005 -i 3  &> logs/grape_avg.log && 
+uv run python -u run_sampling_data.py -p 70 -m "GRAPE_AVG"  -l 0.005 -i 4  &> logs/grape_avg.log && 
+uv run python -u run_sampling_data.py -p 70 -m "GRAPE_AVG"  -l 0.005 -i 5  &> logs/grape_avg.log && 
+uv run python -u run_sampling_data.py -p 70 -m "GRAPE_AVG"  -l 0.05 -i 4  &> logs/grape_avg.log && 
+uv run python -u run_sampling_data.py -p 70 -m "GRAPE_AVG"  -l 0.05 -i 5  &> logs/grape_avg.log  
  '  &> logs/batch1.log &
