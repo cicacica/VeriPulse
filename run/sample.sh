4a2a2839f6e6ebdb2affcab5e12290cb926a06d8
Cica Gustiani <cicagustiani@pm.me>
2026-05-26 18:31:37 +0200

    before handover

--- run/sample.sh
+++ run/sample.sh
@@ -2,7 +2,6 @@
 # [0.06,0.04,5,0.5,0.1,0.05,0.01,0.025,0.005,0.0005
 
 nohup bash -c '
-uv run python -u run_sampling_data.py -p 70 -dum -m "GRAPE_AVG"  -l 0.04 -n 5  &> logs/grape_avg.log  &&
 uv run python -u run_sampling_data.py -p 70 -dum -m "GRAPE_AVG"  -l 0.01 -n 5  &> logs/grape_avg.log  &&
 uv run python -u run_sampling_data.py -p 70 -dum -m "GRAPE_AVG"  -l 0.025 -n 5  &> logs/grape_avg.log  
  '  &> logs/batch1.log &
