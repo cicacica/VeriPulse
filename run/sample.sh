f45644fd4013db8e1c7beae4a35733bac619dd73
Cica Gustiani <cicagustiani@pm.me>
2026-06-19 14:19:11 +0200

    GRAPE AVG lam 0.001

--- run/sample.sh
+++ run/sample.sh
@@ -3,5 +3,5 @@
 # 0.01, 0.05, 0.10, 0.20
 
 nohup bash -c '
-uv run python -u run_sampling_data.py -p 120 -m "GRAPE_AVG" -l 0.05 -det 0.01 -i 2 &> logs/grape_avg.log  
+uv run python -u run_sampling_data.py -p 40 -m "GRAPE_AVG" -l 0.001 -n 5 &> logs/grape_avg.log  
  '  &> logs/batch1.log &
---
+++ run/sample2.sh
@@ -0,0 +1,6 @@
+# run in serial
+nohup bash -c '
+uv run python -u run_sampling_data.py -p 70 -m "GRAPE_AVG"  -l 0.025 -i 3  &> logs/grape_avg.log  &&
+uv run python -u run_sampling_data.py -p 70 -m "GRAPE_AVG"  -l 0.025 -i 4  &> logs/grape_avg.log  &&
+uv run python -u run_sampling_data.py -p 70 -m "GRAPE_AVG"  -l 0.025 -i 5  &> logs/grape_avg.log  
+ '  &> logs/batch2.log &
