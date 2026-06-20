07766c8f1367cc9830c686976ad34de87bea5640
Cica Gustiani <cica.gustiani@lip6.fr>
2026-06-20 23:50:07 +0200

    grape avg

--- run/submit.sh
+++ run/submit.sh
@@ -7,19 +7,22 @@ cd "$(dirname "$0")"
 # err 0.05, 0.1
 #
 # Core 3x3 grid: CRAB/GRAPE
-for D in 0.0 0.05 0.10 0.2; do
-  for err in 0.0 0.05 0.1; do
-    oarsub -l /nodes=1/core=24,walltime=150:0:0 -p "host like 'small%'" \
-           "$(pwd)/sweep.sh $D $err"
-  done
-done
+#for D in 0.0 0.05 0.10 0.2; do
+#  for err in 0.0 0.05 0.1; do
+#    oarsub -l /nodes=1/core=24,walltime=150:0:0 -p "host like 'small%'" \
+#           "$(pwd)/sweep.sh $D $err"
+#  done
+#done
 
 # Stress test
 #oarsub -l /nodes=1/core=48,walltime=24:0:0 -p "host like 'big%'" \
 #       "$(pwd)/sweep.sh 0.20 0.0"
 #
 
-#for rid in 1 2 3 4 5; do
-#    oarsub -l /nodes=1/core=64,walltime=24:0:0 -p "host like 'tall%'" \
-#           "$(pwd)/sweep.sh $rid"
-#done
+# lambda sweep 
+# dummyless [0.005, 0.01 , 0.025, 0.04 , 0.05 , 0.06 , 0.1  , 0.5  , 5.   ])
+# dummyyes  [0.0005,0.005,0.01,0.025,0.026,0.05,0.1,0.5,5]
+for lam in 0.005 0.01 0.025 0.04 0.05 0.06 0.1 0.5 5.0 ; do
+    oarsub -l /nodes=1/core=64,walltime=10:0:0 -p "host like 'tall%'" -n "lamb warm grape dummy yes" \
+           "$(pwd)/sweep.sh $lam"
+done
