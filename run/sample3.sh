f8126f8d42abdd94e03ce75337f72686b49e0fae
Cica Gustiani <cicagustiani@pm.me>
2026-06-24 16:17:40 +0200

    warm crab again

---
+++ run/sample3.sh
@@ -0,0 +1,22 @@
+# run in serial
+#lam40_dummyless = np.sort([0.005,0.01,0.025,0.04,0.05,0.06,0.1,0.5,5])
+#lam40_dummyyes = np.sort([0.0005,0.005,0.01,0.025,0.026,0.05,0.1,0.5,5])
+
+nohup bash -c '
+uv run python -u run_warm_start.py -p 40 -s "CRAB" -dum -l 0.005  -n 5   &> logs/wcrab_avg.log && 
+uv run python -u run_warm_start.py -p 40 -s "CRAB" -dum -l 0.01  -n 5   &> logs/wcrab_avg.log && 
+uv run python -u run_warm_start.py -p 40 -s "CRAB" -dum -l 0.025  -n 5   &> logs/wcrab_avg.log && 
+uv run python -u run_warm_start.py -p 40 -s "CRAB" -dum -l 0.026  -n 5   &> logs/wcrab_avg.log && 
+uv run python -u run_warm_start.py -p 40 -s "CRAB" -dum -l 0.05  -n 5   &> logs/wcrab_avg.log && 
+uv run python -u run_warm_start.py -p 40 -s "CRAB" -dum -l 5  -n 5   &> logs/wcrab_avg.log && 
+uv run python -u run_warm_start.py -p 40 -s "CRAB" -dum -l 0.1  -n 5   &> logs/wcrab_avg.log && 
+uv run python -u run_warm_start.py -p 40 -s "CRAB" -dum -l 0.5  -n 5   &> logs/wcrab_avg.log && 
+uv run python -u run_warm_start.py -p 40 -s "GRAPE" -dum -l 0.05  -n 5   &> logs/wgrape_avg.log && 
+uv run python -u run_warm_start.py -p 40 -s "GRAPE" -dum -l 0.01  -n 5   &> logs/wgrape_avg.log && 
+uv run python -u run_warm_start.py -p 40 -s "GRAPE" -dum -l 0.025  -n 5   &> logs/wgrape_avg.log && 
+uv run python -u run_warm_start.py -p 40 -s "GRAPE" -dum -l 0.026  -n 5   &> logs/wgrape_avg.log && 
+uv run python -u run_warm_start.py -p 40 -s "GRAPE" -dum -l 0.05  -n 5   &> logs/wgrape_avg.log && 
+uv run python -u run_warm_start.py -p 40 -s "GRAPE" -dum -l 0.1  -n 5   &> logs/wgrape_avg.log && 
+uv run python -u run_warm_start.py -p 40 -s "GRAPE" -dum -l 0.5  -n 5   &> logs/wgrape_avg.log && 
+uv run python -u run_warm_start.py -p 40 -s "GRAPE" -dum -l 5  -n 5   &> logs/wgrape_avg.log 
+ '  &> batch.log &
